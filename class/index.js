05f8a6406331d296ecb63ca96966af8ae7ebcce8
Daniel-Oshione <omobsdaniel@gmail.com>
2024-10-10 13:57:56 +0100

    did stuff

---
+++ class/index.js
@@ -0,0 +1,32 @@
+//generating random otp numbers
+const otpKeys = '0123456789';
+const optLen = 10;
+let otpValue = '';
+for(let i = 0; i < optLen; i++){
+    otpValue += otpKeys[Math.floor(Math.random() * 10)];
+}
+console.log(otpValue);
+
+
+//generating random alpha numerical values
+const alphaNums = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const alphanumsLen = 20;
+let value = '';
+for(let i = 0; i < alphanumsLen; i++){
+    value += alphaNums[Math.floor(Math.random() * alphaNums.length)];
+}
+console.log(value);
+
+//arranging an array in descending order with arrow function
+const numb = [8,10,5,7,2,94,64,3,7,14,59,-2];
+numb.sort((b,a)=> a - b);
+console.log(numb);
+
+//arranging in ascending order
+const numbers = [8,10,5,7,2,94,64,3,7,14,59,-2];
+numbers.sort((y,z)=>{
+if (y > z) return 1;
+if (y < z) return -1;
+return 0;
+});
+console.log(numbers);
